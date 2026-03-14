c5f06cd5060a9fe76fc25059c06ed022846f623c
SoudaHimavamsi <vamsihima71@gmail.com>
2026-03-14 21:18:57 +0530

    Time: 4 ms (81.45%), Space: 46.7 MB (22.33%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char [] char_s = s.toCharArray();
+        char [] char_t = t.toCharArray();
+
+        Arrays.sort(char_s);
+        Arrays.sort(char_t);
+
+        if(Arrays.equals(char_s,char_t)){
+            return true;
+        }
+        return false;
+    }
+}
