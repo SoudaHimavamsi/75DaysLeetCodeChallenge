56d99777674207783c797e789d33287f67005108
Souda Himavamsi <vamsihima71@gmail.com>
2026-04-22 21:54:00 +0530

    Time: 0 ms (100%), Space: 45.2 MB (52.31%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,30 +1,16 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+
 class Solution {
     public boolean isValidBST(TreeNode root) {
-        return vbst(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
     }
 
-    static boolean vbst(TreeNode root, long min,long max){
-        if(root==null){
+    private boolean isValidBST(TreeNode node, long min, long max) {
+        if (node == null) {
             return true;
         }
-        if(root.val <= min || root.val>=max){
+        if (node.val <= min || node.val >= max) {
             return false;
         }
-        return vbst(root.left,min,root.val) && vbst(root.right,root.val,max);
+        return isValidBST(node.left, min, node.val) && isValidBST(node.right, node.val, max);
     }
 }
