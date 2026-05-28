f2e1ebe5de099035aaefe3d67a36ae1970516ecf
Souda Himavamsi <vamsihima71@gmail.com>
2026-05-28 17:11:18 +0530

    Time: 2 ms (99.78%), Space: 52.8 MB (11.64%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,30 +1,30 @@
 class Solution {
-    public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
+    public int numIslands(char[][] grids) {
+        int m = grids.length;
+        int n = grids[0].length;
 
         int count = 0;
 
         for(int i = 0;i<m;i++){
             for(int j= 0;j<n;j++){
-                if(grid[i][j] =='1'){
+                if(grids[i][j] =='1'){
                     count++;
-                    Traverse4Dir(grid,i,j);
+                    Traverse4Dir(grids,i,j);
                 }
             }
         }
         return count;
     }
 
-    static void Traverse4Dir(char[][] grid, int i,int j){
-        if(i<0 || j< 0 || i>=grid.length || j>=grid[0].length || grid[i][j] =='0'){
+    static void Traverse4Dir(char[][] grids, int i,int j){
+        if(i<0 || j< 0 || i>=grids.length || j>=grids[0].length || grids[i][j] =='0'){
             return;
         }
-        grid[i][j] = '0';
+        grids[i][j] = '0';
 
-            Traverse4Dir(grid,i,j+1);
-            Traverse4Dir(grid,i+1,j);
-            Traverse4Dir(grid,i,j-1);
-            Traverse4Dir(grid,i-1,j);
+            Traverse4Dir(grids,i,j+1);
+            Traverse4Dir(grids,i+1,j);
+            Traverse4Dir(grids,i,j-1);
+            Traverse4Dir(grids,i-1,j);
     }
 }
