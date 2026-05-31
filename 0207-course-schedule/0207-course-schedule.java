40c03e50e658e43909b91cc34c422644171ff0e5
Souda Himavamsi <vamsihima71@gmail.com>
2026-05-31 20:44:30 +0530

    Time: 6 ms (73.26%), Space: 46.8 MB (86.56%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,31 +1,40 @@
 class Solution {
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        List<List<Integer>> adj = new LinkedList<>();
-        for(int i = 0;i<numCourses;i++){
-            adj.add(new LinkedList<>());
+    public boolean canFinish(int n, int[][] prerequisites) {
+        List<Integer>[] adj = new List[n];
+        int[] indegree = new int[n];
+        List<Integer> ans = new ArrayList<>();
+
+        for (int[] pair : prerequisites) {
+            int course = pair[0];
+            int prerequisite = pair[1];
+            if (adj[prerequisite] == null) {
+                adj[prerequisite] = new ArrayList<>();
+            }
+            adj[prerequisite].add(course);
+            indegree[course]++;
         }
-        int ind[] = new int[numCourses];
 
-        for(int[] arr: prerequisites){
-            adj.get(arr[1]).add(arr[0]);
-            ind[arr[0]]++;
-        }
-
-        Queue<Integer> q = new LinkedList<>();
-
-        for(int i = 0;i<numCourses;i++){
-            if(ind[i] == 0) q.offer(i);
-        }
-        int numCourses_completed =0;
-        while(!q.isEmpty()){
-            int curr = q.poll();
-            numCourses_completed++;
-
-            for(int nei: adj.get(curr)){
-                ind[nei]--;
-                if(ind[nei]==0) q.offer(nei);
+        Queue<Integer> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0) {
+                queue.offer(i);
             }
         }
-        return numCourses == numCourses_completed;
+
+        while (!queue.isEmpty()) {
+            int current = queue.poll();
+            ans.add(current);
+
+            if (adj[current] != null) {
+                for (int next : adj[current]) {
+                    indegree[next]--;
+                    if (indegree[next] == 0) {
+                        queue.offer(next);
+                    }
+                }
+            }
+        }
+
+        return ans.size() == n;
     }
 }
