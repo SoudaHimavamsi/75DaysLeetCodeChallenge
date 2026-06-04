fdb0a0e10fce8d978eae16114bc069f88cf1214b
Souda Himavamsi <vamsihima71@gmail.com>
2026-06-04 22:19:20 +0530

    Time: 0 ms (100%), Space: 42.8 MB (36.07%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,13 +1,13 @@
 class Solution {
     public int rob(int[] nums) {
-        int p2 = 0;
-        int p1 =0;
-        for(int curr : nums){
-    
-            int max_amount = Math.max(curr+p2,p1);
-            p2 = p1;
-            p1 = max_amount;
+        int a = 0, b = 0;
+
+        for (int n : nums) {
+            int temp = Math.max(b, a + n);
+            a = b;
+            b = temp;
         }
-        return p1;
+
+        return b;
     }
 }
